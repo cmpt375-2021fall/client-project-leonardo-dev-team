f090a41af1d2067ffbdb1f632f3d586736b0db07
inexperiencedlightning <inexperiencedlightning@users.noreply.github.com>
2021-09-30 12:23:54 -0600

    Added base page

--- leo/models.py
+++ leo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Calendar(models.Model):
+    title = models.CharField(max_length=64, null=False, blank=False)
+
+    def __str__(self):
+        return self.title
+
