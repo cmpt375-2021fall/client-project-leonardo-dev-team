ca63af5dc952616a966fe83a22b62a57eb8f16b0
g-n <g-n@users.noreply.github.com>
2021-11-18 12:40:48 -0700

    Added login_required decorator to pages that need them. Removed old login and signup views because django-allauth replaces those.

--- leo/views.py
+++ leo/views.py
@@ -3,6 +3,7 @@ from .forms import NewUserForm
 from django.contrib.auth import login
 from django.contrib import messages
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.decorators import login_required
 
 from django.contrib.auth import authenticate
 from django.shortcuts import render, redirect
@@ -15,12 +16,7 @@ from django.http import HttpResponse
 # Create your views here.
 from .models import Post
 
-
-def login(request):
-    print(request.method)
-    return render(request, template_name="login.html")
-
-
+@login_required
 def dashboard(request):
     context = {
 
@@ -28,28 +24,34 @@ def dashboard(request):
     return render(request, 'dashboard.html', context)
 
 
+@login_required
 def newsletter(request):
     context = {}
     return render(request, 'newsletter.html', context)
 
 
+@login_required
 def calendar(request):
     context = {}
     return render(request, 'calendar.html', context)
 
+@login_required
 def account(request):
     context = {}
     return render(request, 'account.html', context)
 
+@login_required
 def exclusive(request):
     context = {}
     return render(request, 'exclusive.html', context)
 
+@login_required
 class exclusiveView(ListView):
     model = Post
     template_name = 'exclusive.html'
     ordering = ['-id']
 
+@login_required
 class exclusiveDetailView(DetailView):
     model = Post
     template_name = 'exclusive_detail.html'
@@ -64,46 +66,3 @@ def footer(request):
     context = {}
     return render(request, 'footer.html', context)
 '''
-
-def register_request(request):
-    if request.method == "POST":
-        form = NewUserForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request)
-            messages.success(request, "Registration successful.")
-            return redirect("login_request")
-        messages.error(request, "Unsuccessful registration. Invalid information.")
-    form = NewUserForm()
-    return render(request=request, template_name="register.html", context={"register_form": form})
-
-def login_request(request):
-    if request.method == "POST":
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                dashboard(request)
-                print(request, f"You are now logged in as {username}.")
-                return redirect("dashboard")
-            else:
-                print(request, "Invalid username or password.")
-        else:
-            print(request, "Invalid username or password.")
-    form = AuthenticationForm()
-    return render(request=request, template_name="login.html", context={"login_form": form})
-
-
-def homepage(request):
-    if request.method == "POST":
-        form = NewUserForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            messages.success(request, "Registration successful.")
-            return redirect("register")
-        messages.error(request, "Unsuccessful registration. Invalid information.")
-    form = NewUserForm()
-    return render(request=request, template_name="register.html", context={"register_form": form})
