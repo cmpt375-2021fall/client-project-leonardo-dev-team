e65a878d995dab58d09f251fb59098d80ea7faf3
inexperiencedlightning <inexperiencedlightning@users.noreply.github.com>
2021-12-02 13:28:13 -0700

    added logout

--- leo/models.py
+++ leo/models.py
@@ -25,10 +25,10 @@ class Calender(models.Model):
 
 class Newsletter(models.Model):
     title = models.CharField(max_length=255)
-    caption = models.CharField(max_length=511)
     header_image = models.ImageField(null=True, blank=True, upload_to="NewsImages/")
     body = models.TextField()
     post_date = models.DateField(default=datetime.now)
+    link = models.CharField(max_length=511, null= True, blank=True)
 
     def __str__(self):
         return self.title
